5637ca72476992902a3bc69d368dcff40504ad67
Riham <r.mohamed@fz-juelich.de>
2026-04-30 11:29:50 +0200

    Add new benchmarkplots script

---
+++ bennchplot/benchmarkplots.py
@@ -0,0 +1,96 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import bennchplot as bp
+import matplotlib as mpl
+from matplotlib import gridspec
+import matplotlib.transforms as mtransforms
+
+path = '/home/mohamed/pythontest/results_file' # with jemalloc
+filename_save = 'NEST-v3.9-rc1_Microcircuit_DOCS_0923.png'
+benchmark_model='microcircuit'
+print('before plot')
+B = bp.Plot(data_file=path, x_axis=['num_nodes'], time_scaling=1e3, detailed_timers= False)
+print('after plot')
+def plot_docs():
+    fig = plt.figure(figsize=(12, 6), constrained_layout=False)
+    spec = gridspec.GridSpec(ncols=2, nrows=1, figure=fig, hspace=0.2)
+
+    ax1 = fig.add_subplot(spec[0, 0])
+
+    trans = mtransforms.ScaledTranslation(-20 /
+                                            72, 7 / 72, fig.dpi_scale_trans)
+
+    B.plot_fractions(axis=ax1,
+                    fill_variables=['time_construction_create+time_construction_connect',
+                        'time_simulate', ],
+                    interpolate=True,
+                    step=None,
+                    error=True)
+   # B.plot_main(quantities=['time_construction_create'], axis=ax1, error=False, fmt='-')
+   # B.plot_main(quantities=['time_construction_connect'], axis=ax1, error=False, fmt= '-')
+
+    ax1.set_xlabel('Number of Nodes')
+    ax1.set_ylabel(r'$T_{\mathrm{wall}}$ [s] for $T_{\mathrm{model}} =$'
+                    + f'{np.unique(B.df.model_time_sim.values)[0]} s')
+
+    handles1, labels1 = ax1.get_legend_handles_labels()
+    ax1.legend(handles1[::-1], labels1[::-1])
+    # Set the ylim for benchmark plots to make different runs of same model visually comparable
+    if benchmark_model == "microcircuit":
+        ax1.set_ylim(0, 15)
+    elif benchmark_model == "hpc_benchmark":
+        ax1.set_ylim(0, 55)
+    elif benchmark_model == "multi_area":
+        ax1.set_ylim(0, 1500)
+    else:
+        pass # Let the ylim creation automatic for potential other models
+
+    ax1.margins(x=0)
+    B.simple_axis(ax1)
+
+    ax2 = fig.add_subplot(spec[0, 1])
+
+    B.plot_main(quantities=['sim_factor'], axis=ax2,
+                error=True, fmt='-')
+
+    ax2.set_xlabel('Number of Nodes')
+    ax2.set_ylabel(r'real-time factor $T_{\mathrm{wall}}/$'
+                    r'$T_{\mathrm{model}}$')
+
+    # Set the xlim
+    automatic_xlim = ax2.get_xlim()
+    ax2.set_xlim(automatic_xlim[0], automatic_xlim[1])
+
+    # Set the xlim
+    automatic_xlim = ax2.get_xlim()
+    ax2.set_xlim(automatic_xlim[0], automatic_xlim[1])
+
+    # Set the ylim for benchmark plots to make different runs of same model visually comparable
+    if benchmark_model == "microcircuit":
+        ax2.set_ylim(0, 1.)
+        # explicitly show realtime factor of 1
+        ax2.hlines(y=1, xmin=0, xmax=np.max(B.df[B.x_axis].values), color='gray', linestyle='--')
+    elif benchmark_model == "hpc_benchmark":
+        ax2.set_ylim(0, 50)
+    elif benchmark_model == "multi_area":
+        ax2.set_ylim(0, 120)
+    else:
+        pass # Let the ylim creation automatic for potential other models
+
+    handles2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(handles2[::-1], labels2[::-1])
+
+    ax2.margins(x=0)
+    B.simple_axis(ax2)
+
+    """
+    ax1.text(0.0, 1.0, 'A', transform=ax1.transAxes + trans,
+            fontsize='medium', va='bottom', fontweight='bold')
+
+    ax2.text(0.0, 1.0, 'B', transform=ax1.transAxes + trans,
+            fontsize='medium', va='bottom', fontweight='bold')
+    """
+
+    plt.savefig(filename_save, dpi=400)
+
+plot_docs()
