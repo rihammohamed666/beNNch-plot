ad7f0ee3fba6baa4ff1457819d3fd4aa731af890
alpinangu <inangualp@gmail.com>
2026-07-15 18:09:13 +0200

    added logging to IO modules

--- bennchplot/io/config/__init__.py
+++ bennchplot/io/config/__init__.py
@@ -1,8 +1,11 @@
 from pathlib import Path
+import logging
 from bennchplot.io.config.models import Config
-
 from bennchplot.io import yaml
 
+log = logging.getLogger(__name__)
+
 def loadConfig(file_name = "config.yaml") -> Config:
+    log.debug("loading config from %s", file_name)
     with Path(file_name).open(encoding="utf8") as in_file:
         return Config.model_validate(yaml.load(in_file))
--- bennchplot/io/tarball/__init__.py
+++ bennchplot/io/tarball/__init__.py
@@ -1,7 +1,11 @@
 from bennchplot.io.config.models import VarConfig, SourceConfig
 from subprocess import check_call
+import logging
+
+log = logging.getLogger(__name__)
 
 def get_variable_value(uuid: str, var: VarConfig, source: SourceConfig) -> None:
     "Get the triggering pipeline ID of the given simulation."
+    log.debug("Getting variable from %s with %s from %s", uuid, var.parser, var.path)
     tar = f'tar --wildcards -zxOf { uuid }.tgz {var.path}'
     check_call(f"ssh {source.host} 'cd {source.path}; { tar } | {var.parser}'", shell=True)
