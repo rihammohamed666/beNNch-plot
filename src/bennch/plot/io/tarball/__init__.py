83014ec1fbc3de2d0d5032699daf01fabc583171
Riham <r.mohamed@fz-juelich.de>
2026-08-19 16:44:14 +0200

    adding a function to handle multiple uuids

--- src/bennch/plot/io/tarball/__init__.py
+++ src/bennch/plot/io/tarball/__init__.py
@@ -3,13 +3,24 @@
 import logging
 from subprocess import check_call
 
-from bennch.plot.io.config.models import SourceConfig, VarConfig
+from bennch.plot.io.config.models import Config, SourceConfig, VarConfig
 
 log = logging.getLogger(__name__)
 
 
-def get_variable_value(uuid: str, var: VarConfig, source: SourceConfig) -> None:
+def get_variable_value(uuid: str, var: VarConfig, source: SourceConfig) -> float:
     "Get the triggering pipeline ID of the given simulation."
     log.debug("Getting variable from %s with %s from %s", uuid, var.parser, var.path)
     tar = f"tar --wildcards -zxOf {uuid}.tgz {var.path}"
-    check_call(f"ssh {source.host} 'cd {source.path}; {tar} | {var.parser}'", shell=True)
+    return float(check_output(f"ssh {source.host} 'cd {source.path}; {tar} | {var.parser}'", shell=True))
+
+
+def get_variables(uuids: list[str], vars: list[str], config: SSConfig):
+    values = {}
+
+    for uuid in uuids:
+        values[uuid] = {}
+
+        for var_name in vars:
+            values[uuid][var_name] = get_variable_value(uuid, config.vars[variable_name], config.source)
+    return values
